963d9835030669f284d94502a013aa3bb023ed58
elberto <elbertoisaac@gmail.com>
2025-01-27 00:33:06 -0300

    Proyecto-Taller-Hemaq-V11

--- funciones.js
+++ funciones.js
@@ -1,3 +1,7 @@
+const HOJA = SpreadsheetApp.openById('1ltyb-txefnjLKR1OnSXOfGOyr-HL0g1mp7oI6aeaDts').getActiveSheet();
+
+
+
 function doGet()
 {
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('TALLER HEMAQ');
@@ -5,14 +9,28 @@ function doGet()
 //aca esta función se activa al iniciar la web y hace lo que esta dentro de la función, que es buscar el archivo con nombre 'web'
 //que sera el archivo con la estructura html de la página
 
+
+function doPost(datos)
+{
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('TALLER HEMAQ');
+}
+
+
 function obtenerDatosHTML(nombre)
 {
     return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
 
+
+//OBTENIENDO DATOS DE CADA HOJA
 function obtenerClientes()
 {
-    let hoja = SpreadsheetApp.openById('1ltyb-txefnjLKR1OnSXOfGOyr-HL0g1mp7oI6aeaDts').getActiveSheet();
-    let datos = hoja.getDataRange().getValues();
-    return datos;
+   return HOJA.getDataRange().getValues();
 }
+
+//INSERTANDO DATOS A CADA HOJA
+function insertarClientes(Tienda, Fecha, Servicio)
+{
+    HOJA.appendRow([Tienda, Fecha, Servicio]);
+}
+
